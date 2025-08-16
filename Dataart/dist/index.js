b71576705289bdd0f211a624c7b6f8e91d3adbdd
vanshjain212 <144898237+vanshjain212@users.noreply.github.com>
2025-08-16 19:20:47 +0530

    Update index.js

--- Dataart/dist/index.js
+++ Dataart/dist/index.js
@@ -1,1 +1,21 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { fetchEvents } from './fetcher.js';
+import { renderTimeline } from './renderer.js';
+document.addEventListener("DOMContentLoaded", () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const events = yield fetchEvents();
+        renderTimeline(events); // This will internally call showModal(event)
+    }
+    catch (err) {
+        console.error("Failed to load events:", err);
+    }
+}));
 
